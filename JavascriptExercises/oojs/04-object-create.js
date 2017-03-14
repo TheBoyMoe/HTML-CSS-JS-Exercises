a79716229c9d78a9d711d5ea883c5d360abdf5be
bill fero <billfero@gmail.com>
2017-03-14 18:23:06 +0000

    impl create method passing in props object

--- JavascriptExercises/oojs/04-object-create.js
+++ JavascriptExercises/oojs/04-object-create.js
@@ -4,10 +4,26 @@
 let Person = {
 	calculateAge: function(){
 		return `Age: ${2016 - this.age}`;
+	},
+	getName: function () {
+		return this.name;
+	},
+	getOccupation: function () {
+		return this.job;
 	}
 };
 
 // create the person instance and populate the properties
+// Object .create builds an object directly from the obj passed in
 let pete = Object.create(Person);
-pete.age = 22;
+pete.name = 'Peter Jones';
+pete.yob = 1948;
 pete.job = 'developer';
+
+
+// 2nd example - passing in obj properties to the create method
+let jane = Object.create(Person, {
+	name: {value: 'jane'},
+	yob: {value: 1966},
+	job: {value: 'developer'}
+});
