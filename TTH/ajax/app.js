3e35dda0ecf26f7a1d70a31f784642d6ffde9b0e
bill fero <billfero@gmail.com>
2017-03-17 14:52:58 +0000

    impl simple ajax request to load web data into target div on page load

--- TTH/ajax/app.js
+++ TTH/ajax/app.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Loaded page content');
 	
+	/* Build an XHR/ajax request */
+	
+	// 1. create the request obj
+	let xhr = new XMLHttpRequest();
+	
+	// 2. create the callback function, the code you want to run when the
+	// response from the server is received - create a function that is
+	// invoked when ever there is a change in the onReadyStateChange event
+	xhr.onreadystatechange = function () {
+		// we're only interested in the final state change, ready state == 4
+		if(xhr.readyState === 4) {
+			document.getElementById('ajax').innerHTML = xhr.responseText;
+		}
+	};
+	
+	// 3. open the request, passing the http method and url (file or service)
+	xhr.open('GET', 'paragraph.html');
+	
+	// 4. send the request - when submitting info, eg form data, pass it to send()
+	xhr.send();
+	
 });
