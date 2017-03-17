498835950b9b3cb2d55c08d99213812c666cd6d8
bill fero <billfero@gmail.com>
2017-03-17 18:53:51 +0000

    use an ajax request to fetch employee json data, build and display list

--- TTH/ajax-json/js/app.js
+++ TTH/ajax-json/js/app.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('Page content loaded');
 	
-	
+	// Build and display list of emplees who are in
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState === 4) {
+			// console.log(xhr.responseText);
+			// JSON object must be an array of items or object of key/value pairs or a combination
+			// otherwise JSON.parse() will retiurn an error
+			var employees = JSON.parse(xhr.responseText);
+			console.table(employees);
+			
+			// iterate through the list of employess and build an ul
+			var list = '<ul class="bulleted">';
+			employees.forEach(function(employee){
+				if(employee.inoffice){
+					list += '<li class="in">';
+				} else {
+					list += '<li class="out">';
+				}
+				list += employee.name + '</li>';
+			});
+			list += '</ul>';
+			document.getElementById('employeeList').innerHTML = list;
+		}
+	};
+	xhr.open('GET', 'data/employees.json');
+	xhr.send();
 	
 });
