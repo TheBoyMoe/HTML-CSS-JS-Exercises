ee7c8545a9c5ed36bb129296d0d23d0c31dca91f
bill fero <billfero@gmail.com>
2017-03-31 20:59:08 +0100

    using the spread operator

--- JavascriptExercises/es6/03-rest-and-spread-syntax.js
+++ JavascriptExercises/es6/03-rest-and-spread-syntax.js
@@ -1,5 +1,5 @@
 /*
- 
+ 	Rest operator
 	The rest parameter syntax allows us to represent an indefinite number of arguments as an array.
 	- ie, take in a series of parameters and treat it like an array
 	[1] https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Functions/rest_parameters
@@ -13,6 +13,17 @@ const fnRest = (...arr)=>{
 	// iterate through the array, sum the values
 	return arr.reduce((prev, curr)=>prev+curr, 0);
 };
-// you can pass in a series of elements, e.g numbers, strings, etc - automatically turned into an array
+// you can pass in a series of elements(any number), e.g numbers, strings, etc - automatically turned into an array
 console.log(fnRest(1,2,3,4,5,6,7));
 
+/*
+	Spread operator
+	- opposite of the rest operator, expand an array passed in as multiple arguments.
+	- use it for functions that expect multiple arguments, eg Math.max
+ */
+let arr = [2,4,3,1,3,5,6,7,4,8,9,5];
+// expands the array into a comma separated list of values that the .max() method is expecting
+console.log('spread', Math.max(...arr));
+
+// in ES5 you could use the apply() method - pass in an array of values to a method that is expecting individual values
+console.log('apply', Math.max.apply(null, arr));
