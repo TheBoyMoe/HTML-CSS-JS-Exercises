8981cac6799626bba009409eb4b7b341efb04489
bill fero <billfero@gmail.com>
2017-04-01 16:01:03 +0100

    using default exports

--- JavascriptExercises/es6/modules/app.js
+++ JavascriptExercises/es6/modules/app.js
@@ -5,9 +5,15 @@
 //import {keyValue} from './external.js';
 // import {fn} from './external.js';
 
-// combine imports - we're importing references to variables, objects, arrays, etc
-// if they are changed somewhere else, the change will be reflected here as well.
+// combine imports
 import {keyValue, fn} from './external.js';
+
+// import default export - you can call the default anything you want
+// - don't have to use the same export name since there can only be one default per file
+import dummyText from './external.js';
+
 
 console.log(`${keyValue}`);
 console.log(fn(3,3));
+
+console.log(dummyText);
--- JavascriptExercises/es6/modules/external.js
+++ JavascriptExercises/es6/modules/external.js
@@ -1,3 +1,5 @@
+
+
 // export let keyValue = 1000;
 
 // export const fn = (a,b)=>{
@@ -9,7 +11,12 @@ let keyValue = 1000;
 
 const fn = (a,b)=>{
 	return a*b;
-};
+}
+
+let text = 'some default text';
 
 // in addition to exporting variables and functions, you can export arrays and objects
 export {keyValue, fn};
+
+// define the default export
+export default text;
