8a3dc725ca9b430d4b71e0ff7f6033fca61ae906
bill fero <billfero@gmail.com>
2017-04-22 20:14:05 +0100

    add a dynamic route to the express config

--- RithmSchoolExercises/express-basics/app.js
+++ RithmSchoolExercises/express-basics/app.js
@@ -1,3 +1,7 @@
+/*
+	References:
+	[1] https://www.rithmschool.com/courses/node-express-fundamentals/introduction-to-express
+ */
 'use strict';
 const express = require('express');
 const app = express();
@@ -6,6 +10,14 @@ const port = process.env.PORT || 3000;
 // GET '/'
 app.get('/', (req, res)=>{
 	res.send('Received your request, now bog off!');
+});
+
+// GET '/instructor/[firstname]
+// To specify that a part of a URL will be a "parameter", add the : character and give
+// the URL parameter a name, e.g firstname in this example. This name will be the key in the params object.
+// ALL url parameters are strings
+app.get('/instructor/:firstname', (req, res)=>{
+	res.send(`the instructors name is ${req.params.firstname}`);
 });
 
 app.listen(port, ()=>{
