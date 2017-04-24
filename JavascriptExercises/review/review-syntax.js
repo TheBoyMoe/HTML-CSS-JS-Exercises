1abf3303cfb039066aad73b398ee5a3666b6a4b4
bill fero <billfero@gmail.com>
2017-04-24 18:40:33 +0100

    object and array destructuring

--- JavascriptExercises/review/review-syntax.js
+++ JavascriptExercises/review/review-syntax.js
@@ -16,6 +16,10 @@ person[last]= 'Smith';
 person[adr] = '1 Pimlico Way, WC1 London';
 person[years] = 44;
 
+person.description = function () {
+	console.log(`name ${this.firstname} ${this.lastname}, age ${this.age}`);
+};
+
 // console.log('----------------------------');
 // console.log(person.firstname, person.lastname, person.address, person.age);
 // console.log('----------------------------');
@@ -45,6 +49,7 @@ for(let num of numbers)
 	console.log(num);
 
 // 5. destructuring of arrays and object
+// object arrays you pull out props by their name, with arrays it's by their position
 
 // assign the first 3 values to a,b,and c. The remaining to d as an array
 let [a,b,c, ...d] = numbers; // pass in the rest parameter
@@ -65,3 +70,37 @@ console.log('j %d, k %d', j, k);
 // you can ignore values in an array
 let [l, ,m, , n, ...o] = numbers; // 23, 3, 12, [53,34,3,45,56,2,124,9]
 console.log('l %d, m %d, n %d, o', l, m, n, o);
+
+
+// 6. destructuring objects - referenced by name, not by position, so you need to use the props name - names must match
+// means order in which you assign the variables does not matter, and you can't leave gaps as you can with arrays
+
+// grab the matching properties and methods from the object
+person.description(); // works
+let {firstname, lastname, age, description} = person;
+console.log(lastname, age);
+// description(); // 'this' is undefined so does not work, even though you can pull out the function
+
+const person2 = {
+	christianName: 'Peter',
+	surname: 'Jones',
+	yrs: 55,
+	info(){
+		console.log(this);
+		console.log(`name ${this.christianName} ${this.surname}, age ${this.yrs}`);
+	},
+	log(){
+		console.log('This should work since we\'re not using this');
+	},
+	log2(){
+		console.log('using alias when destructuring objects');
+	}
+};
+
+// use object destructuring
+// when ever you use aliases, the original variable name is no longer available
+let {christianName: fn, surname: ln, yrs, info, log, log2: logMethod} = person2;
+// info(); // 'this' is undefined when the method is called in this manner
+log(); // this works
+console.log(`name ${fn} ${ln} age: ${yrs}`);
+logMethod();
