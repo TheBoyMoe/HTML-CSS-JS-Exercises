f8628afbcee2b23f1fa6d0377d62640de7ccbd51
bill fero <billfero@gmail.com>
2017-04-24 14:16:17 +0100

    promise chaining, catch, all and race methods

--- JavascriptExercises/es6/018-promises.js
+++ JavascriptExercises/es6/018-promises.js
@@ -3,8 +3,8 @@
 // 1. basic promise, resolve and reject
 const promise = new Promise((resolve, reject)=>{
 	setTimeout(()=>{
-		// resolve('Promise was resolved!');
-		reject(new Error('The promise failed, try again!'));
+		resolve('1. Promise was resolved!');
+		reject(new Error('1. The promise failed, try again!'));
 	}, 1500);
 });
 
@@ -14,3 +14,66 @@ promise.then((str)=>{
 }, (err)=>{
 	console.error(err.message);
 });
+
+
+// 2. chaining promises
+const waitASecond = (seconds)=>{
+	return new Promise((resolve, reject)=>{
+		if(seconds > 2)
+			return reject(new Error('2. Reject!'));
+		setTimeout(() => {
+			seconds++;
+			resolve(seconds);
+		}, 1000);
+		
+	});
+};
+
+waitASecond(2)
+	.then(waitASecond)
+	.then((result)=>{
+		console.log(result); // 2
+	}).catch((err)=>{ // handle errors using catch block
+		console.error(err.message);
+	});
+
+
+// 3.
+const promise1 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve('3a. Resolved');
+	}, 1000);
+});
+
+const promise2 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve('3b. Resolve');
+	}, 2000);
+});
+
+const promise3 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		reject('3c. Rejected');
+	}, 3000);
+});
+
+// all() combines all promises passed to it, waits for ALL of them to be resolved,
+// only then is it resolved - returning the result of all of them
+// if any promise in the chain is rejected, that error is caught and returned to
+// the caller - none of them will resolve successfully
+Promise.all([promise1, promise3, promise2])
+	.then((success)=>{
+		console.log(success);
+	})
+	.catch((error)=>{
+		console.error(error);
+	});
+
+// race() returns the result of the promise that completes first - whether success/failure
+Promise.race([promise1, promise3, promise2])
+	.then((success)=>{
+		console.log(success);
+	})
+	.catch((error)=>{
+		console.error(error);
+	});
