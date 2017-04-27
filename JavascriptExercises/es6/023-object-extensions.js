925a147758d345904dd1628e047911554a600ace
bill fero <billfero@gmail.com>
2017-04-27 19:56:03 +0100

    using Object setPropertyOf

--- JavascriptExercises/es6/023-object-extensions.js
+++ JavascriptExercises/es6/023-object-extensions.js
@@ -34,16 +34,40 @@ let obj4 = new Obj4();
 
 // first object is the base object (uses it's prototype), all subsequent objects in the sequence are merged into it
 let obj34 = Object.assign(obj3, obj4);
-console.log(obj34);
+// console.log(obj34);
 
-console.log(obj34 instanceof Obj3); // true
-console.log(obj34 instanceof Obj4); // false
+// console.log(obj34 instanceof Obj3); // true
+// console.log(obj34 instanceof Obj4); // false
 
-console.log(obj34.__proto__ === Obj3.prototype); // true
-console.log(obj34.__proto__ === Obj4.prototype); // false
+// console.log(obj34.__proto__ === Obj3.prototype); // true
+// console.log(obj34.__proto__ === Obj4.prototype); // false
 
 // you can use an empty object as the base
 let obj5 = Object.assign({}, obj1, obj2);
-console.log(obj5);
-console.log(obj5 instanceof Object); // true
-console.log(obj5.__proto__ === Object.prototype); // true
+// console.log(obj5);
+// console.log(obj5 instanceof Object); // true
+// console.log(obj5.__proto__ === Object.prototype); // true
+
+
+// 2. Object.setPrototypeOf() - set the prototype of an object after it's been created
+//		- there's already Object.create() which allows you to set the prototype at the time of object creation.
+let person = {
+	name: 'John Smith'
+};
+
+let employee = {
+	name: 'Peter Jones',
+	job: 'Developer'
+};
+
+console.log(person.__proto__ === Object.prototype); // true
+console.log(employee.__proto__ === Object.prototype); // true
+
+// reset the prototype of an object - sets the prototype of person to the employee object
+Object.setPrototypeOf(person, employee);
+console.log(person.__proto__ === Object.prototype); // false
+console.log(person.__proto__ === employee); // true
+
+console.log(person); // {name : 'John Smith}
+
+
