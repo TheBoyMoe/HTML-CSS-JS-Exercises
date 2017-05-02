35f578c86db6373827f82b936422ad2a69b20549
bill fero <billfero@gmail.com>
2017-05-02 19:46:37 +0100

    classes and inheritance

--- JavascriptExercises/review/review-classes-2.js
+++ JavascriptExercises/review/review-classes-2.js
@@ -40,3 +40,49 @@ console.log(employee.describe());
 console.log(employee.info());
 
 
+/* ES6 Classes */
+class Animal {
+    // required if you want to give someone properties
+    constructor(species, weight, height, color){
+        this.species = species;
+        this.weight = weight;
+        this.height = height;
+        this.color = color;
+    }
+    
+    info(){
+        return `${this.species}, weight ${this.weight}, height ${this.height}, color ${this.color}`;
+    }
+    
+    run(){
+        console.log(`I can run quickly even though I weigh ${this.weight}`);
+    }
+}
+
+let gorilla = new Animal('Gorilla', '599lbs', '172cm', 'Black');
+console.log(gorilla.info());
+gorilla.run();
+
+// classes and inheritance
+class Ape extends Animal {
+    // you only need to call the constructor if you want to set 'Ape' specific properties
+    // if yoy=u call the constructor, you ust also call the parent's constructor via a call to super
+}
+
+let ape = new Ape('Ape', '340lbs', '123cm', 'brown');
+console.log(ape.info());
+ape.run();
+
+class Chimpanzee extends Animal {
+    constructor(name, species, weight, height, color){
+        super(species, weight, height, color);
+        this.name = name;
+    }
+    // override the parents info() method, to refer to info(), call this.info() from within the class
+    info(){
+        return `My name is ${this.name}, ${super.info()}`; // call parent's info()
+    }
+}
+
+let chimp = new Chimpanzee('Cheeta', 'Chimpanzee', '65lbs', '46cm', 'black');
+console.log(chimp.info());
