60069fc4a5fd1b36a9bc9879e191accd43a81980
bill fero <billfero@gmail.com>
2017-05-03 16:33:39 +0100

    Rithm school express exercise - basic shopping list

---
+++ RithmSchoolExercises/express-basics-exercise/app.js
@@ -0,0 +1,30 @@
+"use strict";
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const items = ['apples', 'pears', 'oranges', 'lemons', 'blueberries'];
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// GET /
+app.get('/', (req, res)=>{
+    res.render('list', {items});
+});
+
+// GET /search
+
+
+// GET /new-item
+
+
+// POST /add-item
+
+
+app.listen(port, ()=>{
+    console.log(`Express is listening on port ${port}`);
+});
