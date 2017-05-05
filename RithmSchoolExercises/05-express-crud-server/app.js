b4b51fe889143c65e59280d6519fe3e49571c0f8
bill fero <billfero@gmail.com>
2017-05-05 13:41:13 +0100

    impl error handling

--- RithmSchoolExercises/05-express-crud-server/app.js
+++ RithmSchoolExercises/05-express-crud-server/app.js
@@ -21,7 +21,14 @@
     
     - the form MUST use the POST method
     
+    
+    References:
+    [1] https://expressjs.com/en/guide/error-handling.html
+    [2] https://www.joyent.com/node-js/production/design/errors
+    [3] https://derickbailey.com/2014/09/06/proper-error-handling-in-expressjs-route-handlers/
+    
  */
+
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -29,18 +36,19 @@ const methodOverride = require('method-override');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'development';
 app.set('view engine', 'pug');
 
-/* middleware */
+let users = [];
+let id = 1;
+
+/* ------------ middleware -------------------- */
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
 
-let users = [];
-let id = 1;
-
-// routes
+/* ------------ routes ------------------------- */
 app.get('/', (req, res, next)=>{
     res.redirect('/users');
 });
@@ -106,7 +114,37 @@ app.delete('/users/:id', (req, res, next)=>{
 });
 
 
+/* handle errors - middleware - need to be defined AFTER your routes */
+
+// catch 404 errors and forward to error handler
+app.use((req, res, next)=>{
+    let error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// dev error handler - will print stack trace
+if(env === 'development'){
+    app.use((err, req, res, next)=>{
+        res.status(err.status || 500);
+        res.render('error', {
+            status: res.statusCode,
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler - no stack trace leaked to user
+app.use((err, req, res, next)=>{
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 app.listen(port, ()=>{
-    console.log(`Express is listening on port ${port}`);
+    console.log(`Express is listening on port ${port}, env #${env}`);
 });
 
