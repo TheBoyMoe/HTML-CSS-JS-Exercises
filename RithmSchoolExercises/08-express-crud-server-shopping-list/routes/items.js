5d3f7bab657764ffed1dce600c397726076601ad
bill fero <billfero@gmail.com>
2017-05-05 18:43:26 +0100

    basic shopplist app implementing CRUD operations

--- RithmSchoolExercises/08-express-crud-server-shopping-list/routes/items.js
+++ RithmSchoolExercises/08-express-crud-server-shopping-list/routes/items.js
@@ -1,1 +1,96 @@
 /* Item routes */
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+let items = [];
+let id = 1;
+
+/* item routes */
+
+// GET /users - display all items
+router.get('/', (req, res, next)=>{
+    // render the index.pug template passing it the items array to be displayed
+    res.render('index', {items: items});
+});
+
+
+// GET /items/new - display the form allowing item to be added
+router.get('/new', (req, res, next)=>{
+    // display the new.pug template
+    res.render('new');
+});
+
+// GET /items/:id - display a shopping list item
+router.get('/:id', (req, res, next)=>{
+    // retrieve the item with the matching id, and display
+    let item = items.find((item)=>{
+        return item.id === Number(req.params.id);
+    });
+    if(item){
+        res.render('show', {item: item});
+    } else {
+        let err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    }
+});
+
+// GET /items/:id/edit - display a form allowing list item to be changed
+router.get('/:id/edit', (req, res, next)=>{
+    let item = items.find((item)=>{
+        return item.id === Number(req.params.id);
+    });
+    if(item){
+        res.render('edit', {item: item});
+    } else {
+        let err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    }
+});
+
+
+// POST /items - save the submitted list item, via '/items/new', to the array
+router.post('/', (req, res, next)=>{
+    items.push({
+        name: req.body.name,
+        id: id
+    });
+    id++;
+    res.redirect('/items');
+});
+
+
+// PATCH /items/:id - save the updated item to the array
+router.patch('/:id', (req, res, next)=>{
+    let item = items.find((item)=>{
+        return item.id === Number(req.params.id);
+    });
+    if(item){
+        item.name = req.body.name; // update the item in the list
+        res.redirect('/items');
+    } else {
+        let err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    }
+});
+
+// POST /items/:id - delete item from the array - uses http DELETE
+router.delete('/:id', (req, res, next)=>{
+    let itemIndex = items.findIndex((item)=>{
+        return item.id === Number(req.params.id);
+    });
+    if(itemIndex) {
+        items.splice(itemIndex, 1);
+        res.redirect('/items');
+    } else {
+        let err = new Error('Not found');
+        err.status = 404;
+        next(err);
+    }
+});
+
+module.exports = router;
